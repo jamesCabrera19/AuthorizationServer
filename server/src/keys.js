1bcb659579d285a14a081430f5d52c245e8a8d84
Jaime Cabrera <66804991+jamesCabrera19@users.noreply.github.com>
2025-08-28 06:25:58 -0500

    Update keys.js

--- server/src/keys.js
+++ server/src/keys.js
@@ -1,8 +1,6 @@
-// mongodb cluster credentials
-// user name: JaimeCabrera, password: password12345
+// insert mongodb cluster credentials
+const mongoUri = '';
 
-const mongoUri = `mongodb+srv://JaimeCabrera:password12345@cluster0.qqzlxpe.mongodb.net/?retryWrites=true&w=majority`;
-
-const encryptionKey = "MY_SERCRET_KEY";
+const encryptionKey = '';
 
 module.exports = { encryptionKey, mongoUri };
